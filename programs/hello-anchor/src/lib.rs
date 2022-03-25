216677c774a428578bf0e495d19197ec7821f4ee
JoelCThemmen <joel@onrampinvest.com>
2022-03-25 16:39:46 -0500

    checking in

---
+++ programs/hello-anchor/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod hello_anchor {
+    use super::*;
+
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()>{
+        ctx.accounts.my_account.data = data;
+        Ok(())
+    }
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount{
+    data: u64;
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info>{
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>
+}
+
+pub struct Initialize {}
